11e96f666209dfb8aa66edbecd2902bb77c2b2f9
BrianRid <brianridolce@gmail.com>
2023-06-22 17:37:07 +0200

    feat: cleaning matomo unused package from android

--- android/app/src/main/java/com/nata/MainApplication.java
+++ android/app/src/main/java/com/nata/MainApplication.java
@@ -9,7 +9,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
-import de.bonify.reactnativematomo.MatomoPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +21,6 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          new MatomoPackage();
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
